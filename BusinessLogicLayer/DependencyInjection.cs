d2320b2f1dd801645a13024cfaa2994d3e16b513
Durvesh-09 <durveshzagade1999@gmail.com>
2026-01-07 22:32:53 +0530

    initial commit

---
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.ProductsService.BusinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+        { 
+            // To Do : Add Business logic layer sevices into the IoC Container
+
+            return services;
+        }
+    }
+}
